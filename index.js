90429d5b39e742ccd0977132fc2a6a97c2c847f7
Jonsey <mwjones2186@gmail.com>
2022-09-11 13:08:09 -0700

    modifactions made. adjusted code to function properly

--- index.js
+++ index.js
@@ -1,129 +1,142 @@
 const inquirer = require("inquirer");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const Manager = require('./lib/Manager')
-const generateHTML = require('./src/generateHtml')
+const Manager = require("./lib/Manager");
+const generateHTML = require("./src/generateHtml");
+const fs = require('fs')
 
 const employees = [];
 
-
 const managerQuestions = [
-    {
-        type: 'input',
-        name: 'name',
-        message: 'Please enter your Managers full name.',
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'What is your employee ID?',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email address?',
-    },
-    {
-        type: 'input',
-        name: 'officeNumber',
-        message: 'What is the Mangers office number?', 
-    }
+  {
+    type: "input",
+    name: "name",
+    message: "Please enter your Managers full name.",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is your employee ID?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "What is the Mangers office number?",
+  },
 ];
 
 const engineerQuestions = [
-    {
-        type: 'input',
-        name: 'name',
-        message: 'Please enter your full name.',
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'What is your employee ID?',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email address?',
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'What is the Github address?', 
-    }
+  {
+    type: "input",
+    name: "name",
+    message: "Please enter your full name.",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is your employee ID?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is the Github address?",
+  },
 ];
 const internQuestions = [
-    {
-        type: 'input',
-        name: 'name',
-        message: 'Please enter your full name.',
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'What is your employee ID?',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email address?',
-    },
-    {
-        type: 'input',
-        name: 'school',
-        message: 'What school are you attending or accepted into?', 
-    }
+  {
+    type: "input",
+    name: "name",
+    message: "Please enter your full name.",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is your employee ID?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "What school are you attending or accepted into?",
+  },
 ];
-const menuQuestion= {
-    type: 'list',
-    name: 'menu',
-    message: 'what would you like to do?',
-    choices: ['Add an Engineer', 'Add an Intern', 'Add an Employee' ,'Finished'],
+const menuQuestion = {
+  type: "list",
+  name: "menu",
+  message: "what would you like to do?",
+  choices: ["Add A Engineer", "Add A Intern", "Finished"],
 };
 
-function start(){
-    inquirer.prompt(managerQuestions).then((responses)=>{
-        const manager = new Manager(responses.name, responses.idNumber, responses.email, responses.officeNumber)
-        employees.push(manager)
-    menu()
-
-    })
+function start() {
+  inquirer.prompt(managerQuestions).then((responses) => {
+    const manager = new Manager(
+      responses.name,
+      responses.id,
+      responses.email,
+      responses.officeNumber
+    );
+    employees.push(manager);
+    menu();
+  });
 }
 
-function menu(){
-    inquirer.prompt(menuQuestion).then((responses)=>{
-       switch(responses.menu){
-        case 'Add A Engineer':
-            addEngineer()
+function menu() {
+  inquirer.prompt(menuQuestion).then((responses) => {
+    switch (responses.menu) {
+      case "Add A Engineer":
+        addEngineer();
         break;
-        case 'Add A Intern':
-            addIntern()
+      case "Add A Intern":
+        addIntern();
         break;
-        case 'Finished':
-            generateHTML(employees)
-        default: 
-
-       }
-    })
-};
-
-function addEngineer(){
-    inquirer.prompt(engineerQuestions).then((responses)=>{
-        const engineer = new Engineer(responses.name, responses.idNumber, responses.email, responses.github)
-    employees.push(engineer)
-    menu()
-
-    })
-    
-};
-
-function addIntern(){
-    inquirer.prompt(internQuestions).then((responses)=>{
-        const intern = new Intern(responses.name, responses.idNumber, responses.email, responses.school)
-        employees.push(intern)
-        menu()
-    })
-        
+      default:
+        createTeam();
+    }
+  });
 }
-start()
 
+function addEngineer() {
+  inquirer.prompt(engineerQuestions).then((responses) => {
+    const engineer = new Engineer(
+      responses.name,
+      responses.id,
+      responses.email,
+      responses.github
+    );
+    employees.push(engineer);
+    menu();
+  });
+}
+
+function addIntern() {
+  inquirer.prompt(internQuestions).then((responses) => {
+    const intern = new Intern(
+      responses.name,
+      responses.id,
+      responses.email,
+      responses.school
+    );
+    employees.push(intern);
+    menu();
+  });
+}
+
+function createTeam() {
+ fs.writeFileSync('./dist/index.html',generateHTML(employees), 'utf-8')
+}
+
+start();
--- src/generateHtml.js
+++ src/generateHtml.js
@@ -1,97 +1,99 @@
-const fs = require('fs');
-const Manager = require('../lib/Manager');
-const path = require('path')
 
-const handleEmployees = {
-    Manager: manager => {
-        return `
-        <div class= "employee-card'>
-            <div class= "card-header">
-                <h2 class= "card-title">${manager.getName()}</h2>
-                <h3 class= "card-title">${manager.getRole()}</h3>
-            </div>
-            <div class = "card-body">
-                <ul class = "list-group">
-                    <li class = "list-group-item>ID: ${manager.getId()}</li>
-                    <li class = "list-group-item>EMAIL: <a href="mailto: ${manager.getEmail()}">${manager.getEmail()}</a></li>
-                    <li class = "list-group-item>OFFICE NUMBER: ${manager.getOfficeNumber()}</li>
-                </ul>
-            </div>
-        </div>      
+
+function generateHTML(employees) {
+
+  function managerHtml(manager) {
+    return  `
+    <div class= "employee-card">
+        <div class= "card-header">
+            <h2 class= "card-title">${manager.getName()}</h2>
+            <h3 class= "card-title">${manager.getRole()}</h3>
+        </div>
+        <div class = "card-body">
+            <ul class = "list-group">
+                <li class = "list-group-item">ID: ${manager.getId()}</li>
+                <li class = "list-group-item">EMAIL: <a href="mailto: ${manager.getEmail()}">${manager.getEmail()}</a></li>
+                <li class = "list-group-item">OFFICE NUMBER: ${manager.getOfficeNumber()}</li>
+            </ul>
+        </div>
+    </div>      
+`;
+  }
+
+  function engineerHtml(engineer) {
+    return  `
+    <div class= "employee-card">
+        <div class= "card-header">
+            <h2 class= "card-title">${engineer.getName()}</h2>
+            <h3 class= "card-title">${engineer.getRole()}</h3>
+        </div>
+        <div class = "card-body">
+            <ul class = "list-group">
+                <li class = "list-group-item">ID: ${engineer.getId()}</li>
+                <li class = "list-group-item">EMAIL: <a href="mailto: ${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                <li class = "list-group-item">GITHUB ADDRESS: <a href= "https://github.com/${engineer.getGithub()}">https://github.com/${engineer.getGithub()}"</a></li>
+            </ul>
+        </div>
+    </div>      
     `;
-    },
-    Engineer: engineer => {
-        return `
-        <div class= "employee-card'>
-            <div class= "card-header">
-                <h2 class= "card-title">${engineer.getName()}</h2>
-                <h3 class= "card-title">${engineer.getRole()}</h3>
-            </div>
-            <div class = "card-body">
-                <ul class = "list-group">
-                    <li class = "list-group-item>ID: ${engineer.getId()}</li>
-                    <li class = "list-group-item>EMAIL: <a href="mailto: ${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-                    <li class = "list-group-item>GITHUB ADDRESS: <a href= "https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferer"></a></li>
-                </ul>
-            </div>
-        </div>      
-        `;
-    },
-    Intern: intern => {
-        return `
-        <div class= "employee-card'>
-            <div class= "card-header">
-                <h2 class= "card-title">${intern.getName()}</h2>
-                <h3 class= "card-title">${intern.getRole()}</h3>
-            </div>
-            <div class = "card-body">
-                <ul class = "list-group">
-                    <li class = "list-group-item>ID: ${intern.getId()}</li>
-                    <li class = "list-group-item>EMAIL: ${intern.getEmail()}</li>
-                    <li class = "list-group-item>SCHOOL: ${intern.getSchool()}</li>
-                </ul>
-            </div>
-        </div>      
-    `;
-    },
-    Employee: employee => {
-        return `
-        <div class= "employee-card'>
-            <div class= "card-header">
-                <h2 class= "card-title">${employee.getName()}</h2>
-                <h3 class= "card-title">${employee.getRole()}</h3>
-            </div>
-            <div class = "card-body">
-                <ul class = "list-group">
-                    <li class = "list-group-item>ID: ${employee.getId()}</li>
-                    <li class = "list-group-item>EMAIL: ${employee.getEmail()}</li>
-                </ul>
-            </div>
-        </div>      
-    `;
-    },
+  }
+
+  function internHtml(intern) {
+    return `
+    <div class= "employee-card">
+        <div class= "card-header">
+            <h2 class= "card-title">${intern.getName()}</h2>
+            <h3 class= "card-title">${intern.getRole()}</h3>
+        </div>
+        <div class = "card-body">
+            <ul class = "list-group">
+                <li class = "list-group-item">ID: ${intern.getId()}</li>
+                <li class = "list-group-item">EMAIL: <a href="mailto:${intern.getEmail()}</li>
+                <li class = "list-group-item">SCHOOL: ${intern.getSchool()}</li>
+            </ul>
+        </div>
+    </div>     
+`;
+  }
+
+  const resultHtml = [];
+
+  resultHtml.push(
+    employees
+      .filter((employee) => employee.getRole() === "Manager")
+      .map((manager) => managerHtml(manager))
+  );
+  resultHtml.push(
+    employees
+      .filter((employee) => employee.getRole() === "Engineer")
+      .map((engineer) => engineerHtml(engineer))
+  );
+  resultHtml.push(
+    employees
+      .filter((employee) => employee.getRole() === "Intern")
+      .map((intern) => internHtml(intern))
+  );
+
+  return resultHtml.join('')
 }
-function generateHTML(data){
-    let html = ''
-    // console.log(data)
-    data.forEach(employee => {
-       html += handleEmployees[employee.constructor.name](employee);
-    //    console.log(employee.constructor.name)
-    });
-    let page = `<!DOCTYPE html>
+
+module.exports = (team) => {
+  return `<!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     </head>
     <body>
-      ${html}
+    <header>
+    <h1>Team</h1>
+    </header>
+    <main class="container-fluid">
+      ${generateHTML(team)}
+      </main>
     </body>
     </html>`;
-
-    fs.writeFileSync(path.join(__dirname, '/dist/index.html'), page)
-}
-
-module.exports = generateHTML
+};
